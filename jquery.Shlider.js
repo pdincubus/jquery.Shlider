1859c27199b098bdf8c58cc3603851291e961b4b
Phil Steer <pd@incb.us>
2013-02-21 10:23:02 +0000

    add check for if nav exists

--- jquery.Shlider.js
+++ jquery.Shlider.js
@@ -79,67 +79,69 @@
             }//end autoSlide
 
             if (settings.includeNav == true) {
-
-                //create left and right nav
-                var navHtml = '<span id="' + settings.navPrevId + '">&lt;</span>';
-                if (settings.navIncludeNumSlides == true) {
-                    //do we want to know what slide we're on?
-                    var navHtml = navHtml + '<span class="' + settings.navNumClass + '">1 of ' + slidings + '</span>';
-                }
-                var navHtml = navHtml + '<span id="' + settings.navNextId + '">&gt;</span>';
-
-                //add the nav to the nav element
-                $('#' + settings.navId).append(navHtml);
-                //diable previous to start with, I mean we're already at the beginning.
-                $('#' + settings.navPrevId).addClass('disabled');
-
-                //what happens when we click previous?
-                $('#' + settings.navPrevId).on('click', function() {
-
-                    //interrupt the timer for auto if it's going
-                    if (settings.autoSlide == true) {
-                        clearInterval(autoSlideTimer);
+                //we only need to add it if it doesn't exist
+                if(!$('#' + settings.navPrevId).length) {
+                    //create left and right nav
+                    var navHtml = '<span id="' + settings.navPrevId + '">&lt;</span>';
+                    if (settings.navIncludeNumSlides == true) {
+                        //do we want to know what slide we're on?
+                        var navHtml = navHtml + '<span class="' + settings.navNumClass + '">1 of ' + slidings + '</span>';
                     }
+                    var navHtml = navHtml + '<span id="' + settings.navNextId + '">&gt;</span>';
 
-                    //prevent animation queueing
-                    if ($(slides).is(':animated') === true) {
-                        return;
-                    }
+                    //add the nav to the nav element
+                    $('#' + settings.navId).append(navHtml);
+                    //diable previous to start with, I mean we're already at the beginning.
+                    $('#' + settings.navPrevId).addClass('disabled');
 
-                    if (currentSlide == 1) {
-                        //we're at the beginning, just disable the button
+                    //what happens when we click previous?
+                    $('#' + settings.navPrevId).on('click', function() {
+
+                        //interrupt the timer for auto if it's going
+                        if (settings.autoSlide == true) {
+                            clearInterval(autoSlideTimer);
+                        }
+
+                        //prevent animation queueing
+                        if ($(slides).is(':animated') === true) {
+                            return;
+                        }
+
+                        if (currentSlide == 1) {
+                            //we're at the beginning, just disable the button
+                            if (settings.autoSlide == true) {
+                                autoSlideTimer = setInterval(autoSliding, settings.waitTime);
+                            }
+
+                            return;
+                        }else {
+                            //ensure we don't have any disabled buttons
+                            $('#' + settings.navNextId + ', #' + settings.navPrevId).removeClass('disabled');
+
+                            //do the shhhhlide
+                            $(slides).animate({
+                                left: '+=' + slideWidth * settings.slidesAtOnce
+                            }, settings.animationDuration, settings.slideEasing);
+
+                            //decrement counter
+                            currentSlide--;
+
+                            //update nav counter
+                            if (settings.navIncludeNumSlides == true) {
+                                $('#' + settings.navId + ' .' + settings.navNumClass).text(currentSlide + ' of ' + slidings);
+                            }
+
+                            //disable button if we've just reached the first slide again
+                            if (currentSlide == 1) {
+                                $('#' + settings.navPrevId).addClass('disabled');
+                            }
+                        }
+
                         if (settings.autoSlide == true) {
                             autoSlideTimer = setInterval(autoSliding, settings.waitTime);
                         }
-
-                        return;
-                    }else {
-                        //ensure we don't have any disabled buttons
-                        $('#' + settings.navNextId + ', #' + settings.navPrevId).removeClass('disabled');
-
-                        //do the shhhhlide
-                        $(slides).animate({
-                            left: '+=' + slideWidth * settings.slidesAtOnce
-                        }, settings.animationDuration, settings.slideEasing);
-
-                        //decrement counter
-                        currentSlide--;
-
-                        //update nav counter
-                        if (settings.navIncludeNumSlides == true) {
-                            $('#' + settings.navId + ' .' + settings.navNumClass).text(currentSlide + ' of ' + slidings);
-                        }
-
-                        //disable button if we've just reached the first slide again
-                        if (currentSlide == 1) {
-                            $('#' + settings.navPrevId).addClass('disabled');
-                        }
-                    }
-
-                    if (settings.autoSlide == true) {
-                        autoSlideTimer = setInterval(autoSliding, settings.waitTime);
-                    }
-                });
+                    });
+                }
 
                 //what happens when we click next?
                 $('#' + settings.navNextId).on('click', function() {
